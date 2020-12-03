dc4a561707765d7ceda7e2cec94d47295de3a83a
anyms <mytellee@gmail.com>
2020-12-03 19:46:40 +0530

    changing toString method to stringify

--- src/proto.js
+++ src/proto.js
@@ -22,7 +22,7 @@ export default class Proto {
         return Number(s);
     }
     
-    toString(collec) {
+    stringify(collec) {
         return this.isString(collec) ? collec : JSON.stringify(collec);
     }
     
